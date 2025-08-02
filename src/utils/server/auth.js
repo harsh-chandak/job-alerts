088994998f33305a8653a04ca0848aee8fd5a570
harsh-chandak <harshnchandak@gmail.com>
2025-08-01 18:00:04 -0700

    Changing files for avoidong build failure

--- src/utils/server/auth.js
+++ src/utils/server/auth.js
@@ -6,7 +6,7 @@ import { ObjectId } from 'mongodb';
 const JWT_SECRET = process.env.JWT_SECRET;
 
 export async function verifyToken(req, res, next) {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req?.headers?.authorization?.split(' ')[1];
   if (!token) return res.status(401).json({ error: 'No token provided' });
 
   try {
